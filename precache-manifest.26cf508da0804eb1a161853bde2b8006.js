3dc15a10701fe952659effd33f553fac27a6e7b4
Hamed Behjat <hamed.behjat@gmail.com>
2019-11-21 14:14:59 +0330

    Updates

---
+++ precache-manifest.26cf508da0804eb1a161853bde2b8006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb74b854b635f9dae5279e6c48aa812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b5af213f9d04dbc2b9",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a79443b671c42d19fdcc",
+    "url": "/static/css/main.811b64dc.chunk.css"
+  },
+  {
+    "revision": "47b5af213f9d04dbc2b9",
+    "url": "/static/js/2.99779ef9.chunk.js"
+  },
+  {
+    "revision": "a79443b671c42d19fdcc",
+    "url": "/static/js/main.0868e052.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
